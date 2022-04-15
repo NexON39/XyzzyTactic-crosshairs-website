1c4cd58da758d3d5646ec1c878ae8b775083dde5
Kacper <kscode.pl@gmail.com>
2022-04-15 14:09:07 +0200

    cards update

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,18 @@
 window.addEventListener('scroll', function() {
     let navbar = document.querySelector('.navbar');
     let top_btn = document.querySelector('.top_btn');
+    let footer = document.querySelector('.footer');
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         navbar.style.margin = '0';
         navbar.style.borderRadius = '0';
         navbar.style.padding = '0.5rem 2rem';
         top_btn.style.display = 'block';
+        footer.style.bottom = '0';
     } else {
         navbar.style.margin = '2rem 2rem 0 2rem';
         navbar.style.borderRadius = '20px';
         navbar.style.padding = '0 2rem';
         top_btn.style.display = 'none';
+        footer.style.bottom = '-50px';
     }
 })
